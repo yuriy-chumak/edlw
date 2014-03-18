5f3deed31ff42e7b65f4c71516d3bf2465e823f7
Yuriy Chumak <yuriy.chumak@mail.com>
2014-03-18 22:26:44 +0200

    Added basic sample implementation for automation application testing. Please, check that id in scenario equal to real generated id for target ui element id.

--- src/com/example/edlw/MainActivity.java
+++ src/com/example/edlw/MainActivity.java
@@ -1,11 +1,34 @@
 package com.example.edlw;
 
+import java.io.EOFException;
+import java.io.InputStreamReader;
+import java.io.Reader;
+import java.io.StringReader;
+import java.util.ArrayList;
 import java.util.Calendar;
+import java.util.List;
+
+import org.jatha.Lisp;
+import org.jatha.Tests;
+import org.jatha.compile.LispPrimitive1;
+import org.jatha.compile.LispPrimitive2;
+import org.jatha.dynatype.LispNumber;
+import org.jatha.dynatype.LispSymbol;
+import org.jatha.dynatype.LispValue;
+import org.jatha.dynatype.StandardLispCons;
+import org.jatha.dynatype.StandardLispConstant;
+import org.jatha.dynatype.StandardLispNIL;
+import org.jatha.dynatype.StandardLispSymbol;
+import org.jatha.exception.CompilerException;
+import org.jatha.exception.LispValueNotANumberException;
+import org.jatha.exception.LispValueNotASymbolException;
+import org.jatha.read.LispParser;
 
 import android.os.Bundle;
 import android.app.Activity;
 import android.content.Intent;
 import android.content.pm.ActivityInfo;
+import android.util.Log;
 import android.view.Menu;
 import android.view.View;
 import android.view.View.OnClickListener;
@@ -14,7 +37,20 @@ import android.widget.TimePicker;
 import android.widget.TimePicker.OnTimeChangedListener;
 
 public class MainActivity extends Activity {
-
+	static Lisp lisp = new Lisp();
+	static LispParser cli = new LispParser(lisp, new InputStreamReader(System.in));
+	static {
+/*		while (true) {
+			System.out.print("> ");
+			try {
+				System.out.println(lisp.eval(cli.read()).toString());
+			} catch (EOFException e) {
+				return; // done.
+			}
+		}*/
+	}
+	
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
@@ -50,6 +86,101 @@ public class MainActivity extends Activity {
 				Model.A5 = time.getCurrentHour() * 60 + time.getCurrentMinute();
 			}
 		});
+		
+		
+		lisp.COMPILER.Register(new LispPrimitive1(lisp, "SLEEP") {
+			@Override
+			protected LispValue Execute(LispValue arg) throws CompilerException {
+				if (arg instanceof LispNumber) {
+					int times = (int)((LispNumber)arg).getLongValue();
+					try {
+						Thread.sleep(times);
+					} catch (InterruptedException e) {
+						e.printStackTrace();
+					}
+					return lisp.T;
+				}
+				throw new LispValueNotANumberException(arg);
+			}
+		});
+		lisp.COMPILER.Register(new LispPrimitive1(lisp, "FIND-VIEW-BY-ID") {
+			@Override
+			protected LispValue Execute(LispValue arg) throws CompilerException {
+				if (arg instanceof LispNumber) {
+					int id = (int)((LispNumber)arg).getLongValue();
+					return new LispView(lisp, "view-" + (LispNumber)arg, findViewById(id));
+				}
+				throw new LispValueNotANumberException(arg);
+			}
+		});
+		lisp.COMPILER.Register(new LispPrimitive2(lisp, "SET-VIEW-BACKGROUND-COLOR") {
+			@Override
+			protected LispValue Execute(LispValue arg1, LispValue arg2) throws CompilerException {
+				if (!(arg2 instanceof LispNumber))
+					throw new LispValueNotANumberException(arg2);
+				if (!(arg1 instanceof LispSymbol))
+					throw new LispValueNotASymbolException(arg1);
+				
+				final LispView lview = (LispView)arg1;
+				final int color = (int)((LispNumber)arg2).getLongValue();
+				
+				runOnUiThread(new Runnable() {
+					@Override
+					public void run() {
+						lview.view.setBackgroundColor(0xFF000000 + (color & 0x00FFFFFF));
+						lview.view.invalidate();
+					}});
+				return Lisp.NIL;
+			}
+		});
+		new Thread(new Runnable() {
+			@Override
+			public void run() {
+				String scenario = "tests/scenario1.lisp";
+				Reader resourceReader = new InputStreamReader(
+						MainActivity.class.getClassLoader().getResourceAsStream(scenario)
+				);
+				
+				Log.i("lisp", "Testing " + scenario + " ... ");
+//				List<String> errors = new ArrayList<String>();
+				
+//				Lisp lisp = new Lisp(); 
+				Log.i("lisp", lisp.eval("(defun restart () 'restart)").toString());   // сигнал к перезагрузке интерпретатора
+				
+				LispParser cli = new LispParser(lisp, resourceReader);
+				while (true) {
+					// System.io.printnl();
+					try {
+						LispValue s = cli.read();
+						if (s instanceof StandardLispSymbol) {
+							if (s.toString().equals("RESTART")) {
+								lisp = new Lisp();
+								cli = new LispParser(lisp, resourceReader);
+								continue;
+							}
+						}
+						LispValue r = lisp.eval(s);
+						Log.i("lisp", r.toString());
+						
+						if ((r instanceof StandardLispConstant && !r.toString().equals("T"))
+						 || (r instanceof StandardLispNIL)
+						) {
+							Log.e("lisp", "    FAILED " + s.toString() + " -> " + r.toString());
+						}
+					} catch (EOFException e) {
+						break;
+					}
+				}
+/*				if (errors.size() > 0) {
+					System.out.println("FAILED!");
+					for (String error : errors)
+						System.out.println("    " + error);					
+					System.out.println("!FAILED");					
+				}
+				else
+					System.out.println("Ok");*/
+			}
+		}).start();
 	}
 
 /*	@Override
@@ -58,4 +189,14 @@ public class MainActivity extends Activity {
 		getMenuInflater().inflate(R.menu.main, menu);
 		return true;
 	}*/
+}
+
+class LispView extends StandardLispSymbol
+{
+	View view;
+	public LispView(Lisp lisp, String symbolName, View view) {
+		super(lisp, symbolName);
+		
+		this.view = view;
+	}
 }
---
+++ src/tests/scenario1.lisp
@@ -0,0 +1,14 @@
+(+ 1 2)
+(setq now (find-view-by-id 2131296272))
+
+(setq green 65280)
+(setq red 16711680)
+(setq blue 255)
+
+(set-view-background-color now blue)
+(sleep 1000)
+(set-view-background-color now red)
+(sleep 1000)
+(set-view-background-color now green)
+(sleep 1000)
+(set-view-background-color now 0)
